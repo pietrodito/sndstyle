0dc2442dcf7c9cd2b4ef849d9bf1dfd2e204830c
pietrodito <pietrodito@gmail.com>
2024-03-28 08:25:45 +0100

    Add load_settings_from_csv.R

---
+++ R/load_settings_from_csv.R
@@ -0,0 +1,13 @@
+#'@export
+load_settings_from_csv <- function(filepath) {
+  cli::cli_h1("Chargement des paramètres RStudio {.emph {filepath}}")
+  sndshare::import_from_csv(filepath,
+                            dest_path = "~/.config/",
+                            project_name = "rstudio",
+                            open_project = FALSE,
+                            verbose = FALSE)
+  cli::cli_alert_info(paste(
+    "Vous devez redémarrer complètement RStudio",
+    "pour que vos nouveaux paramètres s'appliquent.")
+  )
+}
